4f8ac2ab66f834ca02438e17dbf9fce67c229a87
liliana2110 <madelianadewi2110@gmail.com>
2024-01-10 08:28:26 +0800

    WAYAN TRAVEL

---
+++ jquery.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // Memberikan efek fadeIn pada gambar dengan class card-img-top
+    $('.card-img-top').hide().fadeIn(1500); // Efek fadeIn selama 1.5 detik (1500 milidetik)
+  });
+
+
