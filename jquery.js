8e3736e1934691362b3357c8eb7e4a9774121af1
liliana2110 <madelianadewi2110@gmail.com>
2024-03-22 18:05:08 +0800

    coba

--- jquery.js
+++ jquery.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // $(document).ready(function() { }): Ini adalah cara untuk menunggu sampai seluruh dokumen HTML selesai dimuat sebelum menjalankan kode JavaScript
     // Memberikan efek fadeIn pada gambar dengan class card-img-top
     $('.card-img-top').hide().fadeIn(1500); // Efek fadeIn selama 1.5 detik (1500 milidetik)
   });
